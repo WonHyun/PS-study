246d0fe6115bd15c87f09cf4d65c162c35b1aa74
최 원현 <coy4728@naver.com>
2019-07-10 15:32:27 +0900

    20190710 lv01 문제추가

--- Programmers/lv01/7.cpp
+++ Programmers/lv01/7.cpp
@@ -1,14 +1,27 @@
 #include "../test-prog.h"
-//#if (PNUM == lv01-7)
-#if 1
-//
+#if (PNUM == lv01-7)
+//서울에서 김서방 찾기
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    vector<string> seoul;
+    seoul.push_back("Jane");
+    seoul.push_back("Kim");
+
+    string answer = "";
+
+    for (int i = 0; i < seoul.size(); i++) {
+        if (seoul[i] == "Kim") {
+            answer += "김서방은 " + to_string(i) + "에 있다";
+        }
+    }
+
+    cout << answer;
 
     return 0;
 }
-
 #endif
