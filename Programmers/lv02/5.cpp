53ad2dc855258d3368b73659acfa4001d59e4f13
최 원현 <coy4728@naver.com>
2019-07-17 16:07:36 +0900

    prog lv02-5 124 나라의 숫자

---
+++ Programmers/lv02/5.cpp
@@ -0,0 +1,35 @@
+﻿#include "../test-prog.h"
+#if (PNUM == lv02-5)
+
+//124 나라의 숫자
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    string arr[4] = { "4", "1" , "2" };
+
+    while (n > 0) {
+        answer += arr[n % 3];
+        if (n % 3 == 0) n--;
+        n /= 3;
+    }
+    reverse(answer.begin(),answer.end());
+    return answer;
+}
+
+int main() {
+
+    for(int i=0;i<20;i++) {
+        cout << i << " : " << solution(i) << endl;
+    }
+   
+    return 0;
+}
+
+#endif
