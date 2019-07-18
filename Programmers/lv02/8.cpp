838a1e5c2b4c034b9ee1a58e2180d53a3046b140
최 원현 <coy4728@naver.com>
2019-07-18 17:52:42 +0900

    prog lv02-8 프린터 문제 추가

---
+++ Programmers/lv02/8.cpp
@@ -0,0 +1,61 @@
+﻿#include "../test-prog.h"
+#if (PNUM == lv02-8)
+
+//프린터 문제
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct document
+{
+    int location, prior;
+};
+
+bool operator<(document a, document b) {
+    return a.prior < b.prior;
+}
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+
+    queue<document> q;
+    priority_queue<document> pq;
+
+    for (int i = 0; i < priorities.size(); i++) {
+        document cur; cur.location = i; cur.prior = priorities[i];
+        q.push(cur);
+        pq.push(cur);
+    }
+
+    while (!q.empty()) {
+        if (q.front().prior < pq.top().prior) {
+            //현재 작업의 우선순위가 최고 우선순위보다 작은 경우
+            q.push(q.front());
+            q.pop();
+        }
+        else {
+            //현재 최고 우선순위 보다 크거나 같은 경우
+            answer++;
+            if (q.front().location == location) break;
+            q.pop();
+            pq.pop();
+            
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> priorities = { 1, 1, 9, 1, 1, 1 };
+    int location = 0;
+
+    cout << solution(priorities, location);
+
+    return 0;
+}
+
+#endif
