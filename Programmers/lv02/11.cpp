936578a95982b3a0887bd43e2ed17d1e1f59a175
최 원현 <coy4728@naver.com>
2019-07-19 15:11:37 +0900

    prog lv02-11 쇠막대기 문제 추가

---
+++ Programmers/lv02/11.cpp
@@ -0,0 +1,48 @@
+﻿#include "../test-prog.h"
+#if (PNUM == lv02-11)
+
+// 쇠막대기 문제
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(string arrangement) {
+    int answer = 0;
+    vector<char> stack;
+    bool isRazer = false;
+
+    for (int i = 0; i < arrangement.length(); i++) {
+        if (arrangement[i] == '(') {
+            stack.push_back(arrangement[i]);
+            isRazer = true;
+        }
+        else {
+            if (!stack.empty()) {
+                stack.pop_back();
+            }
+            if (isRazer) {
+                //레이저 bal4
+                answer += stack.size();
+                isRazer = false;
+            }
+            else {
+                //쇠막대가 끝나는 경우
+                answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    string arrangement = "()(((()())(())()))(())";
+
+    cout << solution(arrangement);
+
+    return 0;
+}
+
+#endif
