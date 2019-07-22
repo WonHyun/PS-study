c98d8199ea8418562a8ee3b642b4f6719f9c4566
최 원현 <coy4728@naver.com>
2019-07-22 10:05:32 +0900

    academy 파일구조 변경 및 ds 테스트 문제 추가

---
+++ 2019-summer-academy/ds/1311.c
@@ -0,0 +1,94 @@
+﻿//ds test2
+#if 0
+
+#include <stdio.h>
+
+int STACK[4];
+int SP = 3;
+
+int PUSH(int d)
+{
+    // 코드설계
+    if (SP == -1) return 0xFF;
+    STACK[SP--] = d;
+    return SP;
+}
+
+int POP(int* p)
+{
+    // 코드설계
+    if (SP == 3) return 0xFF;
+    *p = STACK[++SP];
+    return SP;
+}
+
+// 이부분은설계한코드의오류를확인하기위한테스트코드임수정금지 
+
+int main(void)
+{
+    int i;
+    int r;
+    int d;
+
+    int oper[] = { 1, 1, 1, 1, 1,    0, 0, 0, 0, 0,    1, 1, 1, 0, 0, 1, 1,  0 };
+    int stk[] = { 1, 3, 5, 7, 9,     7, 5, 3, 1, 0,    2, 4, 6, 6, 4, 8, 10, 10 };
+    int res[] = { 2, 1, 0, -1, 0xff, 0, 1, 2, 3, 0xFF, 2, 1, 0, 1, 2, 1, 0,  1 };
+
+    for (i = 0; i < (sizeof(oper) / sizeof(oper[0])); i++)
+    {
+        if (oper[i] == 1)
+        {
+            r = PUSH(stk[i]);
+
+            if (r != res[i])
+            {
+                printf(">>>>Fail: Expected return => %d, Your return => %d\n", res[i], r);
+                return 0;
+            }
+
+            if (r == 0xFF)
+            {
+                printf("return => 0x%.2X\n", r);
+                continue;
+            }
+
+            if (STACK[SP + 1] != stk[i])
+            {
+                printf(">>>>Fail : Expected stack data => %d, Your stack data => %d\n", stk[i], STACK[SP + 1]);
+                return 0;
+            }
+
+            printf("return => 0x%.2X\n", r);
+            printf("PUSH => %d\n", STACK[SP + 1]);
+        }
+
+        else
+        {
+            r = POP(&d);
+
+            if (r != res[i])
+            {
+                printf(">>>>Fail: Expected return => %d, Your return => %d\n", res[i], r);
+                return 0;
+            }
+
+            if (r == 0xFF)
+            {
+                printf("return => 0x%.2X\n", r);
+                continue;
+            }
+
+            if (d != stk[i])
+            {
+                printf(">>>>Fail: Expected POP data => %d, Your POP data => %d\n", stk[i], d);
+                return 0;
+            }
+
+            printf("return => 0x%.2X\n", r);
+            printf("POP => %d\n", d);
+        }
+    }
+
+    return 1;
+}
+#endif
---
+++ 2019-summer-academy/ds/1313.c
@@ -0,0 +1,118 @@
+﻿//ds test4
+#if 0
+#include <stdio.h>
+
+#define STEP   2
+#define HASH   5  
+
+int Hash[9] = { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
+int Insert(int d)
+{
+    // 코드설계
+    int key = d % HASH;
+    int i;
+
+    for (i = 0; i < 10; i += STEP) {
+        if (Hash[key] == -1) {
+            Hash[key] = d;
+            return key;
+        }
+        key = (key + STEP) % 10;
+    }
+    return -1;
+}
+
+int Delete(int d)
+{
+    // 코드설계
+    int i;
+    int key = d % HASH;
+    int idx = -1;
+
+    for (i = 0; i < 10; i += STEP) {
+        if (Hash[key] == d) {
+            idx = i;
+            break;
+        }
+        key = (key + STEP) % 10;
+    }
+
+    if (idx == -1) return -1;
+    Hash[idx] = -1;
+    return idx;
+}
+
+/********************************************************************************/
+/* 이 부분은 설계한 코드의 오류를 확인하기 위한 테스트 코드임 수정금지   */
+/********************************************************************************/
+
+int main(void)
+{
+    int i;
+    int r;
+
+    int oper[] = { 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,   1, 0, 0,  1,  1 };
+    int hash[] = { 2, 0, 5, 4, 7, 9, 9,  8,  15, 17, 21, 2, 31, 18, 50 };
+    int res[] = { 2, 0, 4, 6, 8, 1, -1, 3,   5, 7,  -1, 2, -1, 2,  -1 };
+
+    for (i = 0; i < (sizeof(oper) / sizeof(oper[0])); i++)
+    {
+        printf("TEST VECTOR [%d]\n", i);
+
+        if (oper[i] == 1)
+        {
+            r = Insert(hash[i]);
+
+            if (res[i] != r)
+            {
+                printf(">>>>Fail: Expected return => %d, Your return=> %d\n", res[i], r);
+                return 0;
+            }
+
+            else
+            {
+                if ((r != -1) && (hash[i] != Hash[r]))
+                {
+                    printf(">>>>Fail: Expected HASH data => %d, Your HASH data => %d\n", hash[i], Hash[r]);
+                    return 0;
+                }
+
+                else
+                {
+                    if (r != -1) printf("return => %d, value => %d\n", r, Hash[r]);
+                    else printf("return => %d\n", r);
+                }
+            }
+        }
+
+        else
+        {
+            r = Delete(hash[i]);
+
+            if (res[i] != r)
+            {
+                printf(">>>>Fail: Expected return => %d, Your return=> %d\n", res[i], r);
+                return 0;
+            }
+
+            else
+            {
+                if ((r != -1) && (Hash[r] != -1))
+                {
+                    printf(">>>>Fail: Expected HASH data => %d, Your HASH data => %d\n", hash[i], Hash[r]);
+                    return 0;
+                }
+
+                else
+                {
+                    if (r != -1) printf("return => %d\n", r);
+                    else printf("return => %d\n", r);
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+#endif
