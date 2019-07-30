7d8321c83fe9db599055adbe00f178be70e2f6c2
최 원현 <coy4728@naver.com>
2019-07-30 15:43:31 +0900

    study algo 피보나치 재귀 가지치기 추가

--- 2019-summer-academy/algo (advanced)/1709.cpp
+++ 2019-summer-academy/algo (advanced)/1709.cpp
@@ -5,23 +5,32 @@
 
 using namespace std;
 
-//// 재귀함수 버전
-//int fibonacci(int n) {
+int arr[41];
+
+// 가지친 재귀함수 버전
+int fibonacci(int n) {
+    if (arr[n]) return arr[n];
+    if (n == 1 || n == 2) return 1;
+    return arr[n] = fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+//// 일반 재귀함수 버전
+//int fibonacci3(int n) {
 //    if (n == 1 || n == 2) return 1;
 //    return fibonacci(n - 2) + fibonacci(n - 1);
 //}
 
 // 반복문 버전
-int fibonacci(int n) {
-    int fibo = 0, first = 1, second = 1;
-    if (n == 1 || n == 2) return 1;
-    for (int i = 3; i <= n; i++) {
-        fibo = first + second;
-        first = second;
-        second = fibo;
-    }
-    return fibo;
-}
+//int fibonacci(int n) {
+//    int fibo = 0, first = 1, second = 1;
+//    if (n == 1 || n == 2) return 1;
+//    for (int i = 3; i <= n; i++) {
+//        fibo = first + second;
+//        first = second;
+//        second = fibo;
+//    }
+//    return fibo;
+//}
 
 int main() {
     int n;
