e5eb31e1350a51f3eadb7143079a78292323ceb1
최 원현 <coy4728@naver.com>
2019-07-30 14:48:49 +0900

    study algo 피보나치 수열 문제 추가

---
+++ 2019-summer-academy/algo (advanced)/1709.cpp
@@ -0,0 +1,35 @@
+#include "test-algo.h"
+#if (PNUM == 1709)
+
+// 피보나치 수열 문제 
+
+using namespace std;
+
+//// 재귀함수 버전
+//int fibonacci(int n) {
+//    if (n == 1 || n == 2) return 1;
+//    return fibonacci(n - 2) + fibonacci(n - 1);
+//}
+
+// 반복문 버전
+int fibonacci(int n) {
+    int fibo = 0, first = 1, second = 1;
+    if (n == 1 || n == 2) return 1;
+    for (int i = 3; i <= n; i++) {
+        fibo = first + second;
+        first = second;
+        second = fibo;
+    }
+    return fibo;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n);
+
+    return 0;
+}
+
+#endif
