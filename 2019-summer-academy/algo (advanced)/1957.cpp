23524e543932a4dd8dd5e2c288cec9ca9c6d7a94
최 원현 <coy4728@naver.com>
2019-07-31 11:23:07 +0900

    study algo 주사위 던지기 2 순열 버전 추가

--- 2019-summer-academy/algo (advanced)/1957.cpp
+++ 2019-summer-academy/algo (advanced)/1957.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int n, m;
 
-int a[8]; // 최대 주사위 던지는 횟수는 7
+int a[8]; // 주사위를 던지는 횟수는 최대 7번
+bool check[7]; // 순열용 중복 베제
 
 void dice(int count, int sum) {
     if (count > n) {
@@ -27,10 +28,32 @@ void dice(int count, int sum) {
     }
 }
 
+void diceP(int count, int sum) {
+    if (count > n) {
+        if (sum == m) {
+            for (int i = 1; i <= n; i++) {
+                cout << a[i] << " ";
+            }
+            cout << endl;
+        }
+
+        return;
+    }
+    for (int i = 1; i <= 6; i++) {
+        if (check[i]) continue; // 중복을 막는다
+        a[count] = i;
+        check[i] = true;
+        diceP(count + 1, sum + i);
+        check[i] = false;
+    }
+}
+
 int main() {
     cin >> n >> m;
 
     dice(1, 0);
+    cout << endl;
+    diceP(1, 0);
 
     return 0;
 }
