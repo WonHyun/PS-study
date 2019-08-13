fbe453e70d74de36f07c67037a31b194039e59c3
최 원현 <coy4728@naver.com>
2019-08-13 13:42:29 +0900

    prog lv02-12 큰수 만들기 문제 솔루션 추가

--- Programmers/lv02/12.cpp
+++ Programmers/lv02/12.cpp
@@ -31,11 +31,36 @@ string solution(string number, int k) {
     return answer;
 }
 
+// 좋은 풀이
+
+string solution2(string number, int k) {
+    string answer = "";
+    answer = number.substr(k); // {2, 5, 2, 8, 4, 1}
+    for (int i = k - 1; i >= 0; i--) { // i = 3, 2, 1, 0 => {7, 7, 1, 4}
+        int j = 0;
+        do {
+            // 해당 자리에 있는 수가 제자리를 찾아갈 때 까지 루프
+            if (number[i] >= answer[j]) {
+                char temp = answer[j];
+                answer[j] = number[i];
+                number[i] = temp;
+                j++;
+            }
+            else {
+                break;
+            }
+        }while (1);
+    }
+
+    return answer;
+}
+
 int main() {
     string number = "4177252841";
     //string number = "1231234";
 
     cout << solution(number, 4);
+    //cout << solution2(number, 4);
 
     return 0;
 }
