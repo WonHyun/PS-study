6dbd3291fbd6b35d4e54450e3ef226a6bddc3e0f
WonHyun <coy4728@naver.com>
2019-10-17 21:01:58 +0900

    프린터 문제 수정

--- etc/nav/3.cpp
+++ etc/nav/3.cpp
@@ -32,19 +32,33 @@ vector<int> solution(vector<vector<int>> data) {
         q.push(doc);
     }
 
-    while (true) {
+    pq.push(q.front());
+    curTime = q.front().time;
+    q.pop();
+
+    while (!q.empty() || !pq.empty()) {
+        document cur;
         if (pq.empty()) {
-            document cur = q.front(); q.pop();
+            cur = q.front(); q.pop();
+            curTime += cur.time + cur.page - curTime;
+        }
+        else {
+            cur = pq.top(); pq.pop();
             curTime += cur.page;
         }
-        
+        result.push_back(cur.num);
+        while (!q.empty() && q.front().time <= curTime) {
+            pq.push(q.front()); q.pop();
+        }
     }
 
     return result;
 }
 
 int main() {
-    vector<vector<int>> data = { {1, 0, 5}, {2, 2, 2}, {3, 3, 1}, {4, 4, 1}, {5, 10, 2} };
+    //vector<vector<int>> data = { {1, 0, 5}, {2, 2, 2}, {3, 3, 1}, {4, 4, 1}, {5, 10, 2} }; // 1, 3, 4, 2, 5
+    //vector<vector<int>> data = { {1, 0, 3}, {2, 1, 3}, {3, 3, 2}, {4, 9, 1}, {5, 10, 2} }; // 1, 3, 2, 4, 5
+    vector<vector<int>> data = { {1, 2, 10}, {2, 5, 8}, {3, 6, 9}, {4, 20, 6}, {5, 25, 5} }; // 1, 2, 4, 5, 3
     vector<int> result;
 
     result = solution(data);
